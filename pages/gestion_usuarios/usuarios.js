91bfa75932c18f303fb7cdea47b9441f60f4b1e5
Michelle Patiño <thocokun@gmail.com>
2021-11-30 23:01:54 -0600

    Bitacora terminada

--- pages/gestion_usuarios/usuarios.js
+++ pages/gestion_usuarios/usuarios.js
@@ -1,56 +1,112 @@
 import Scaffold from "../../components/layout/Scaffold";
 import IBMDataTable from "../../components/Tabla/IBMDataTable";
-import Table  from "../../components/Tabla/IBMDataTable";
+import React, { useRef } from "react";
+import { BiArchive,BiUserPlus } from "react-icons/bi";
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,Button,Input
+  } from '@chakra-ui/react'
+
+
+
 function usuarios() {
-    let rutas = [
-        {
-          url: "/gestion_de_usuarios/usuarios",
-          nombre: "Usuarios",
-          isCurrentPage: true,
-        },
-      ];
-
-      const rows = [
-        {
-          id: "a",
-          name: "Load balancer 1",
-          status: "Disabled",
-        },
-        {
-          id: "b",
-          name: "Load balancer 2",
-          status: "Starting",
-        },
-        {
-          id: "c",
-          name: "Load balancer 3",
-          status: "Active",
-        },
-      ];
-    
-      const headers = [
-        {
-          key: "name",
-          header: "Name",
-        },
-        {
-          key: "status",
-          header: "Status",
-        }
-      ];
- 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
 
 
 
-    return (
-        <Scaffold  
-        rutas={rutas}
-        titulo="Gestion de Usuarios"
-        descripcion="Usuarios">
-  
-        <IBMDataTable m={2}  headers={headers} rows={rows} ></IBMDataTable>
-        </Scaffold>
-    );
+
+  let rutas = [
+    {
+      url: "/gestion_de_usuarios/usuarios",
+      nombre: "Usuarios",
+      isCurrentPage: true,
+    },
+  ];
+
+  const rows = [
+    {
+      id: "a",
+      name: "Laura Gomez",
+      status: "Deshabilitado",
+      rol: "Administrador",
+    },
+    {
+      id: "b",
+      name: "Joe Sierra",
+      status: "Activo",
+      rol: "Capturador",
+    },
+    {
+      id: "c",
+      name: "Emmanuel Esquivel",
+      status: "Activo",
+      rol: "Director",
+    },
+  ];
+
+  const headers = [
+    {
+      key: "name",
+      header: "Nombre",
+    },
+    {
+      key: "status",
+      header: "Estado",
+    },
+    {
+      key: "rol",
+      header: "Rol",
+    },
+    {
+      key: "configuration",
+      header: "Configuracion",
+    },
+
+  ];
+
+  return (
+    <Scaffold rutas={rutas} titulo="Gestion de Usuarios" descripcion="Usuarios">
+
+
+<Button m ={5} rightIcon= {<BiArchive size = "40px "/>} ref={btnRef} background='gray.200'   onClick={onOpen}>
+        Actividad mas reciente...
+      </Button>
+      <Button  leftIcon={<BiUserPlus size = "40px "/>} colorScheme='teal' url = "/agregar_usuarios">
+        Agregar nuevo usuario
+      </Button>      
+      
+      <Drawer onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Ultima Actividad</DrawerHeader>
+          <DrawerBody>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Salir
+            </Button>
+            <Button colorScheme='blue'>Exportar</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+
+
+      <IBMDataTable headers={headers} rows={rows}></IBMDataTable>
+
+
+
+    </Scaffold>
+  );
 }
 
 export default usuarios;
