2f00e1c8119608b5faf4ffb00bf50868f1b597bb
Michelle Patiño <thocokun@gmail.com>
2021-11-30 21:02:48 -0600

    Se agrego tabla de usuarios
    Falta la información de los usuarios

--- pages/gestion_usuarios/usuarios.js
+++ pages/gestion_usuarios/usuarios.js
@@ -1,8 +1,55 @@
+import Scaffold from "../../components/layout/Scaffold";
+import IBMDataTable from "../../components/Tabla/IBMDataTable";
+import Table  from "../../components/Tabla/IBMDataTable";
 function usuarios() {
+    let rutas = [
+        {
+          url: "/gestion_de_usuarios/usuarios",
+          nombre: "Usuarios",
+          isCurrentPage: true,
+        },
+      ];
+
+      const rows = [
+        {
+          id: "a",
+          name: "Load balancer 1",
+          status: "Disabled",
+        },
+        {
+          id: "b",
+          name: "Load balancer 2",
+          status: "Starting",
+        },
+        {
+          id: "c",
+          name: "Load balancer 3",
+          status: "Active",
+        },
+      ];
+    
+      const headers = [
+        {
+          key: "name",
+          header: "Name",
+        },
+        {
+          key: "status",
+          header: "Status",
+        }
+      ];
+ 
+
+
+
     return (
-        <div>
-            Usuarios
-        </div>
+        <Scaffold  
+        rutas={rutas}
+        titulo="Gestion de Usuarios"
+        descripcion="Usuarios">
+  
+        <IBMDataTable m={2}  headers={headers} rows={rows} ></IBMDataTable>
+        </Scaffold>
     );
 }
 
