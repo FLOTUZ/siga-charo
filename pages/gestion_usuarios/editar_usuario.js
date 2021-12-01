0d026c878de95d5e573de51c73504f1bb71a9823
Michelle Patiño <thocokun@gmail.com>
2021-11-30 20:09:33 -0600

    Gestion de usuarios / Editar usuarios terminado

--- pages/gestion_usuarios/editar_usuario.js
+++ pages/gestion_usuarios/editar_usuario.js
@@ -4,8 +4,6 @@ import {
   FormErrorMessage,
   FormHelperText,
   Box,
-  Grid,
-  GridItem,
   Text,
   Button,
   Heading,
@@ -13,55 +11,164 @@ import {
   Spacer,
   Input,
   Select,
+  InputGroup,
+  MdBuild,
+  Editable,
+  EditablePreview,
+  EditableInput,Switch
 } from "@chakra-ui/react";
+import Scaffold from "../../components/layout/Scaffold";
 
 function editar_usuario() {
+  let rutas = [
+    {
+      url: "/gestion_de_usuarios",
+      nombre: "editar usuarios",
+      isCurrentPage: true,
+    },
+  ];
+
   return (
-    <div className="container">
-      <Heading as="h1" color="gray.500" fontSize="4xl">
-        Gestion de Usuarios
-      </Heading>
-      <Text color="gray.500" fontSize="2xl">
-        Agregar Usuarios
-      </Text>
-      <Box
-        p="100"
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-      >
-        <Box w="100%" p={1}></Box>
-        <Box>
-          <Flex height="100vh" w="100vh" justifyContent="center">
-            <Flex
-              
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              w="100vh"
-              background="gray.200"
-              p={12}
+    <Scaffold
+      rutas={rutas}
+      titulo="Gestion de Usuarios"
+      descripcion="Editar usuario"
+    >
+      <Box bg="white" w="100%" p={2}></Box>
+
+      <Box m={5}>
+        <Flex m={2}>
+          <Flex
+            borderStyle="solid"
+            borderColor="gray.200"
+            borderWidth="2px"
+            direction="column"
+            w="100%"
+            p={10}
+            rounded={6}
+          >
+            <Heading color="gray" as="h3" fontSize="2xl">
+              Datos del usuario
+            </Heading>
+            <Text m={1}>Nombre(s)</Text>
+            <Editable placeholder="Nombre(s)" />
+            <Editable
+              borderStyle="solid"
               rounded={6}
+              borderWidth="1px"
+              color="gray.500"
+              m={2}
+              defaultValue="Juan"
             >
-              <FormControl id="country">
-                <FormLabel>Rol del Usuario</FormLabel>
-                <Select placeholder="Rol...">
-                  <option>Capturador</option>
-                  <option>Administrador</option>
-                  <option>Director</option>
-                </Select>
-              </FormControl>
+              <EditablePreview />
+              <EditableInput />
+            </Editable>
+
+            <Text>Apellidos</Text>
+            <Editable
+              borderStyle="solid"
+              rounded={6}
+              borderWidth="1px"
+              color="gray.500"
+              m={2}
+              defaultValue="Gomez"
+            >
+              <EditablePreview />
+              <EditableInput />
+            </Editable>
+
+
+            <Flex borderStyle="solid"
+            borderColor="gray.200"
+            borderWidth="2px"
+            background="gray.200"
+            direction="column"
+            w="100%"
+            p={4}
+            rounded={6}>
+
+         <FormControl display="flex">
+            <FormLabel >
+              Habilitar opciones
+            </FormLabel>
+            <Switch  />
+          </FormControl>
+            <FormControl m={1} id="country">
+              <FormLabel m={2}>Rol del Usuario</FormLabel>
+              <Select m={2} placeholder="Rol...">
+                <option>Capturador</option>
+                <option>Administrador</option>
+                <option>Director</option>
+              </Select>
+            </FormControl>
             </Flex>
+        
+
+
+
           </Flex>
 
-          <Button size="lg" colorScheme="green" mt="24px">
-            Create a free account
+          <Box
+            m={10}
+            bg="purple.200"
+            w=".1%"
+            direction="column"
+            justifyContent="Center"
+          ></Box>
+
+          <Flex
+            borderStyle="solid"
+            borderColor="gray.200"
+            borderWidth="2px"
+            background="gray.200"
+            direction="column"
+            w="100%"
+            p={12}
+            rounded={6}
+          >
+            <Heading
+              m={2}
+              justifyContent="Left"
+              color="gray"
+              as="h3"
+              fontSize="2xl"
+            >
+              Detalles de login
+
+              <FormControl display="flex" alignItems="center">
+            <FormLabel htmlFor="email-alerts" mb="0">
+              editar opciones de login 
+            </FormLabel>
+            <Switch id="email-alerts" />
+          </FormControl>
+            </Heading> 
+
+            <FormControl id="email">
+              <FormLabel>Correo Electronico</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormLabel>Contraseña</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="4.5rem"
+                type={"password"}
+                placeholder="Enter password"
+              />
+            </InputGroup>
+
+          </Flex>
+        </Flex>
+
+        <Box direction="column" w="100%" p={5} rounded={6}>
+          <Button m={1} colorScheme="teal" variant="solid">
+            Guardar
+          </Button>
+          <Button m={1} colorScheme="teal" variant="outline">
+            Cancelar
           </Button>
         </Box>
       </Box>
-    </div>
+    </Scaffold>
   );
 }
-
 export default editar_usuario;
