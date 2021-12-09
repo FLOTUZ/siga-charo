fec1871d22aff335d5af192b81a7ae4b89cb465a
Emmanuel <flotuz10@gmail.com>
2021-12-09 14:03:07 -0600

    Graficos de dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -16,6 +16,9 @@ import {
 import { AiFillFileAdd } from "react-icons/ai";
 
 import { Consultar } from "../services/API";
+import Linea from "../components/graficas/Linea";
+import VerticalBar from "../components/graficas/VerticalBar";
+import BarAgrupada from "../components/graficas/BarAgrupada";
 
 function Dashboard() {
   const [usuarioLoguado, setUsuarioLoguado] = useState({});
@@ -34,7 +37,9 @@ function Dashboard() {
           estatus: "pendiente",
         },
       });
-      setSolicitudesEnEspera(respuesta.data.length);
+      respuesta.status === 200
+        ? setSolicitudesEnEspera(respuesta.data.length)
+        : setSolicitudesEnEspera("Error");
     };
 
     const aprobados = async () => {
@@ -43,7 +48,10 @@ function Dashboard() {
           estatus: "aprobado",
         },
       });
-      setSolicitudesAprobadas(respuesta.data.length);
+
+      respuesta.status === 200
+        ? setSolicitudesAprobadas(respuesta.data.length)
+        : setSolicitudesAprobadas("Error");
     };
 
     enEspera();
@@ -126,10 +134,7 @@ function Dashboard() {
           </Link>
         </Wrap>
         <AspectRatio ratio={16 / 9}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
-            alt="demo"
-          />
+          <BarAgrupada/>
         </AspectRatio>
       </Scaffold>
     </>
