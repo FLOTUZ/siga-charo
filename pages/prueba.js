96f2f1ec00af0cd90a99ead02e40a0f2c1bf248c
Emmanuel <flotuz10@gmail.com>
2021-12-09 22:19:11 -0600

    Graficas e impresion de  solicitudes

---
+++ pages/prueba.js
@@ -0,0 +1,12 @@
+import { imprimir } from "../utils/Utils";
+import Solicitud from "../components/reportes/Solicitud/Solicitud";
+import { useEffect } from "react";
+function Prueba() {
+  return (
+    <>
+      <Solicitud id="solicitud" />
+    </>
+  );
+}
+
+export default Prueba;
