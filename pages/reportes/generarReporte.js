b54499a1d69460a4b537d46a5eea6c0ee1570858
Emmanuel <flotuz10@gmail.com>
2021-12-14 13:40:58 -0600

    Class por classname y formateo de codigo

--- pages/reportes/generarReporte.js
+++ pages/reportes/generarReporte.js
@@ -1,94 +1,75 @@
 import Scaffold from "../../components/layout/Scaffold";
-import {  
-    Button,
-  } from "@chakra-ui/react";
-  
+import { Button } from "@chakra-ui/react";
+
 let rutas = [
-    {
-        url: "/Reportes",
-        nombre: "Generar Reporte",
-        isCurrentPage: true,
-    },
+  {
+    url: "/Reportes",
+    nombre: "Generar Reporte",
+    isCurrentPage: true,
+  },
 ];
 
-
-
 function Reportes() {
-    return (
-        <Scaffold
-            titulo="Reporte"
-            descripcion="Generación de Reporte"
-            rutas={rutas}>
+  return (
+    <Scaffold
+      titulo="Reporte"
+      descripcion="Generación de Reporte"
+      rutas={rutas}
+    >
+      <body>
+        <div className="inicio1">SOLICITUD DE APOYO</div>
+        <div className="inicio2">AYUNTAMIENTO DE CHARO</div>
 
+        <div className="subtitulo">
+          Charo, Michoacan con fecha DD mes de MM del año 20YY
+          <br />
+          Lea detenidamente su informacion y verifique que sus datos se han
+          escrito correctamente.
+          <br />
+          Datos personales:
+        </div>
 
-            <>
+        <div className="datosPersonales">
+          Nombre: Nombre del usuario
+          <br />
+          Apellido: Apellido Paterno Apellido Materno
+          <br />
+          Direccion: Direccion del usuario
+          <br />
+          Numero: 01-900-XXX
+          <br />
+          Sexo: F/M/NA
+          <br />
+          Fecha de nacimiento: DD/MM/YYYY
+          <br />
+          Ciudad: Ciudad
+          <br />
+          Estado: Estado
+          <br />
+          <br />
+        </div>
+        <br />
+        <div className="fin1">
+          Para la solicitud del apoyo SIGA-CHARO
+          <br />
+          Los datos anteriormente puestos en este documento se utilizaran para
+          el tramite de solicitud de apoyo
+          <br />
+          Brindado por el Ayuntamiento De Charo. Toda la informacion debera ser
+          validada para su confirmacion.
+          <br />
+          <br />
+          <br />
+          <br />
+          FIRMA DE LA INSTITUCION
+        </div>
+      </body>
 
-                <html lang="en">
-
-                    <body>
-                        <div class="inicio1">
-                            SOLICITUD DE APOYO
-                        </div>
-                        <div class="inicio2">
-                        AYUNTAMIENTO DE CHARO
-                        </div>
-                            
-                    
-                        <div class="subtitulo">
-                            Charo, Michoacan con fecha DD mes de MM del año 20YY
-                            <br />
-                            Lea detenidamente su informacion y verifique que sus datos se han escrito correctamente.
-                            <br />
-                            Datos personales:
-                        </div>
-                        
-                        
-
-                        <div class="datosPersonales">
-                            Nombre: Nombre del usuario
-                            <br />
-                            Apellido: Apellido Paterno Apellido Materno
-                            <br />
-                            Direccion: Direccion del usuario
-                            <br />
-                            Numero: 01-900-XXX
-                            <br />
-                            Sexo: F/M/NA
-                            <br />
-                            Fecha de nacimiento: DD/MM/YYYY
-                            <br />
-                            Ciudad: Ciudad
-                            <br />
-                            Estado: Estado
-                            <br />
-                            <br />
-                        </div>
-                        <br />
-                        <div class="fin1">
-                            Para la solicitud del apoyo SIGA-CHARO
-                            <br />
-                            Los datos anteriormente puestos en este documento se utilizaran para el tramite de solicitud de apoyo
-                            <br />
-                            Brindado por el Ayuntamiento De Charo. Toda la informacion debera ser validada para su confirmacion.
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            FIRMA DE LA INSTITUCION
-                        </div>
-                    </body>
-                </html>
-
-
-                <Button
-                colorScheme="blue"
-                variant="solid">
-                Imprimir
-              </Button>
-
-            </>
-        </Scaffold>
-    );
+      <Button colorScheme="blue" variant="solid">
+        Imprimir
+      </Button>
+    </Scaffold>
+  );
 }
 
 export default Reportes;
