a8ec1c5e28dd480a786bbdc9afab541ff76eb535
Jaquelyne <jaquelynevazquez1999@gmail.com>
2021-12-14 13:35:48 -0600

    Css y html para generar reporte

---
+++ pages/reportes/generarReporte.js
@@ -0,0 +1,94 @@
+import Scaffold from "../../components/layout/Scaffold";
+import {  
+    Button,
+  } from "@chakra-ui/react";
+  
+let rutas = [
+    {
+        url: "/Reportes",
+        nombre: "Generar Reporte",
+        isCurrentPage: true,
+    },
+];
+
+
+
+function Reportes() {
+    return (
+        <Scaffold
+            titulo="Reporte"
+            descripcion="Generación de Reporte"
+            rutas={rutas}>
+
+
+            <>
+
+                <html lang="en">
+
+                    <body>
+                        <div class="inicio1">
+                            SOLICITUD DE APOYO
+                        </div>
+                        <div class="inicio2">
+                        AYUNTAMIENTO DE CHARO
+                        </div>
+                            
+                    
+                        <div class="subtitulo">
+                            Charo, Michoacan con fecha DD mes de MM del año 20YY
+                            <br />
+                            Lea detenidamente su informacion y verifique que sus datos se han escrito correctamente.
+                            <br />
+                            Datos personales:
+                        </div>
+                        
+                        
+
+                        <div class="datosPersonales">
+                            Nombre: Nombre del usuario
+                            <br />
+                            Apellido: Apellido Paterno Apellido Materno
+                            <br />
+                            Direccion: Direccion del usuario
+                            <br />
+                            Numero: 01-900-XXX
+                            <br />
+                            Sexo: F/M/NA
+                            <br />
+                            Fecha de nacimiento: DD/MM/YYYY
+                            <br />
+                            Ciudad: Ciudad
+                            <br />
+                            Estado: Estado
+                            <br />
+                            <br />
+                        </div>
+                        <br />
+                        <div class="fin1">
+                            Para la solicitud del apoyo SIGA-CHARO
+                            <br />
+                            Los datos anteriormente puestos en este documento se utilizaran para el tramite de solicitud de apoyo
+                            <br />
+                            Brindado por el Ayuntamiento De Charo. Toda la informacion debera ser validada para su confirmacion.
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            FIRMA DE LA INSTITUCION
+                        </div>
+                    </body>
+                </html>
+
+
+                <Button
+                colorScheme="blue"
+                variant="solid">
+                Imprimir
+              </Button>
+
+            </>
+        </Scaffold>
+    );
+}
+
+export default Reportes;
